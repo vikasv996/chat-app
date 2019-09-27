1bcec2b69994fbe9c6e4d19d7982822d61f7cb95
Vikas Valechha <vikas@acquire.io>
2019-09-27 12:48:22 +0530

    Add socket conn

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const http = require('http');
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '/../public');
+const server = http.createServer(app);
+const io = socketIO(server);
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(port, (error) => {
+server.listen(port, (error) => {
     if (error) {
         console.error("Error connecting");
         process.exit(0);
     }
     console.log(`Server connected to ${port}`);
+});
+
+io.on('connection', (socket) => {
+    console.log("New user connected");
+
+    socket.on('disconnect', () => {
+        console.log("User disconnected");
+    })
 });
